cdbb3c7b1bcc15061a7c391451093c49966e78c0
Marat Khanbekov <marat.khanbekov@gmail.com>
2021-02-07 12:04:38 +0300

    init 4 lesson

---
+++ fourthLesson/fourthLesson/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  fourthLesson
+//
+//  Created by Marat Khanbekov on 07.02.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ fourthLesson/fourthLesson/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  fourthLesson
+//
+//  Created by Marat Khanbekov on 07.02.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
