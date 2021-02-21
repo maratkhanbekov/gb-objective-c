1997a828a5641c189c1a6f8ae9282a6de503d1d0
Marat Khanbekov <marat.khanbekov@gmail.com>
2021-02-21 14:18:47 +0300

    replcae to new PingPongGame

---
+++ PingPongGame/PingPongGame/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PingPongGame
+//
+//  Created by Marat Khanbekov on 21.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PingPongGame/PingPongGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PingPongGame
+//
+//  Created by Marat Khanbekov on 21.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+
+@end
+
